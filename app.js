d91693d383bb0feb51771e996ac56b579462b66c
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-12-26 21:21:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,20 @@ console.log('Server Started on port ' + port.address().port);
 io = require('socket.io')(serv,{upgradeTimeout:360000});
 io.sockets.on('connection',function(socket){
 	socket.on('signIn',function(data){
-		console.log(data.username);
+		var d = new Date();
+		var m = '' + d.getMinutes();
+		var h = d.getHours() + 24;
+		if(SERVER !== 'localhost'){
+			h -= 5;
+		}
+		h = h % 24;
+		h = '' + h;
+		if(m.length === 1){
+			m = '' + 0 + m;
+		}
+		if(m === '0'){
+			m = '00';
+		}
+		console.error("[" + h + ":" + m + "] " + data.username);
 	});
 });
